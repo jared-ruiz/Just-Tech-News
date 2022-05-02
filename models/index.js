eeda30842fe2911e109477f54411424d2051b13e
Jared Ruiz <jared707@gmail.com>
2022-05-01 20:03:23 -0500

    added Post model.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 //import User and export it with it as a property
-const User = require('./user')
+const User = require("./user");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
