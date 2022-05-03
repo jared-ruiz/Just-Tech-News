b1c97ac02bf9e22f845e37a97e5c58f8f8834418
Jared Ruiz <jared707@gmail.com>
2022-05-02 23:41:46 -0500

    Added static function within Post model, added Vote model and its routes as well. Set up relationship between Post and User.

--- models/index.js
+++ models/index.js
@@ -1,14 +1,54 @@
 //import User and export it with it as a property
+const Vote = require('./Vote');
 const Post = require("./Post");
 const User = require("./User");
 
 // create association 
+//User can have many posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 })
 
+//only one post can belong to a user
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 })
 
-module.exports = { User, Post };
+///////////////////////////////////
+ //A user can belong to many voted on posts
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+})
+
+//voted posts can belong to many users who have voted on them
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+////////////////////////////////////
+
+//Only one vote belongs to a user
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+//only one vote can be added to a post
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+//a user can vote on many posts
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+})
+
+//a post can have many votes
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+//importing sequelize to query total votes in our vote put
+const sequelize = require('../../config/connection');
 
 //GET ALL users
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         //order when retrieving data DESC = descending 
         order: [['created_at', 'DESC']],
         include: [
@@ -28,7 +35,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -61,6 +74,18 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     })
+})
+
+//PUT /api/posts/upvote
+//this must be before the put /:id route so express doesnt think that route is valid for this one
+router.put('/upvote', (req, res) => {
+    //custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 })
 
 //update
