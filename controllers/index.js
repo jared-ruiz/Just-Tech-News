cda333831419a32db73b43f8a4aa8428725806bc
Jared Ruiz <jared707@gmail.com>
2022-05-07 15:36:09 -0500

    Added handlebars to project, established homepage.handlebars and main template.

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 //collect packaged group of API endpoints and prefixing them with path '/api'
-
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
