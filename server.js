c6f56d06ce8116745c6e560d9462843ec7890cca
Jared Ruiz <jared707@gmail.com>
2022-05-08 02:50:06 -0500

    added session module, cookies, and session deletion.

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//sessions and sequelize-store
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
