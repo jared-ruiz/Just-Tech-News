9b463219eda12befe0ccda3f149a9956e03a7c0f
Jared Ruiz <jared707@gmail.com>
2022-05-08 16:39:48 -0500

    Added comment and upvote front end javascript, added handlebars functionality to the respective pages.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,13 +95,17 @@ router.post('/', (req, res) => {
 //PUT /api/posts/upvote
 //this must be before the put /:id route so express doesnt think that route is valid for this one
 router.put('/upvote', (req, res) => {
-    //custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    //make sure session exists first
+    if (req.session) {
+        //pass session id along with all destrucured propertoes on req.body
+        //custom static method created in models/Post.js
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 })
 
 //update
--- models/Post.js
+++ models/Post.js
@@ -4,8 +4,8 @@ const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {
     static upvote (body, models) {
-        return model.Vote.create({
-            user_id: body.user.id,
+        return models.Vote.create({
+            user_id: body.user_id,
             post_id: body.post_id
         })
         .then(() => {
